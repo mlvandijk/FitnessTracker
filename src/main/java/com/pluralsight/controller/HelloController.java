437f41365702b522d24aec82c74024c01e81ad92
Marit van Dijk <maritvandijk@bol.com>
2017-09-28 21:06:45 +0200

    Double check everything is same as in tutorial

--- src/main/java/com/pluralsight/controller/HelloController.java
+++ src/main/java/com/pluralsight/controller/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HelloController {
 
     @RequestMapping(value="/greeting")
-    public String sayHello(Model model){
+    public String sayHello(Model model) {
         model.addAttribute("greeting", "Hello World");
         return "hello";
     }
